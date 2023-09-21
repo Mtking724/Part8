bbfd92e2e232ea76691fbdfd9d1114713ac5910e
motoki0724 <motokiokayasu@gmail.com>
2023-09-21 21:37:52 +0900

    Initial Commit

---
+++ Part8/Part8App.swift
@@ -0,0 +1,17 @@
+//
+//  Part8App.swift
+//  Part8
+//
+//  Created by Motoki Okayasu on 2023/09/21.
+//
+
+import SwiftUI
+
+@main
+struct Part8App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
